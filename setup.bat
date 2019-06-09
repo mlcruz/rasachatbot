1bab0d440c8f064d61b21a7d1220c69a132afa1c
Matheus Cruz <mlcruz@inf.ufrgs.br>
2019-06-09 17:30:18 -0300

    project init

--- setup.bat
+++ setup.bat
@@ -1,4 +1,9 @@
-call %cd%\env\Scripts\activate.bat
-call curl https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.pt.300.vec.gz --output "pt_vectors"
-call pip install -r requirements.txt
-call pip install rasa-x --extra-index-url https://pypi.rasa.com/simple
+REM python -m venv env
+REM call %cd%\env\Scripts\activate.bat
+REM call curl https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.pt.300.vec.gz --output "pt_vectors.vec.gz"
+REM call pip install -r requirements.txt
+REM call pip install rasa-x --extra-index-url https://pypi.rasa.com/simple
+REM mkdir tmp\pt_vectors_wiki_lg
+REM python -m spacy init-model pt tmp/pt_vectors_wiki_lg --vectors-loc pt_vectors.vec.gz
+REM python -m spacy link tmp/pt_vectors_wiki_lg pt
+REM rasa init --no-prompt
