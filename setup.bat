a239751276b3abd18d1126b5969105496b70ae24
mlcruz1 <mlcruz@inf.ufrgs.br>
2019-06-10 14:05:12 -0300

    setup update

--- setup.bat
+++ setup.bat
@@ -1,9 +1,13 @@
-REM python -m venv env
-REM call %cd%\env\Scripts\activate.bat
-REM call curl https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.pt.300.vec.gz --output "pt_vectors.vec.gz"
-REM call pip install -r requirements.txt
-REM call pip install rasa-x --extra-index-url https://pypi.rasa.com/simple
-REM mkdir tmp\pt_vectors_wiki_lg
-REM python -m spacy init-model pt tmp/pt_vectors_wiki_lg --vectors-loc pt_vectors.vec.gz
-REM python -m spacy link tmp/pt_vectors_wiki_lg pt
-REM rasa init --no-prompt
+python -m venv env
+call %cd%\env\Scripts\activate.bat
+call curl https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.pt.300.vec.gz --output "pt_vectors.vec.gz"
+call pip install -r requirements.txt
+call pip install rasa-x --extra-index-url https://pypi.rasa.com/simple
+mkdir tmp\pt_vectors_wiki_lg
+python -m spacy init-model pt tmp/pt_vectors_wiki_lg --vectors-loc pt_vectors.vec.gz
+python -m spacy link tmp/pt_vectors_wiki_lg pt
+rasa init --no-prompt
+call del /f config.yml
+call copy config_default.yml config.yml
+call rmdir /S /Q models
+rasa train
