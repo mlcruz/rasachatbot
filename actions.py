aeba3eb130dc13f7b65cda83d7e9d2638c3460b3
Matheus Cruz <mlcruz@inf.ufrgs.br>
2019-06-17 08:56:28 -0300

    sanity check finished

--- actions.py
+++ actions.py
@@ -7,6 +7,7 @@ from rasa_sdk.forms import FormAction
 import requests
 import json
 
+
 class ActionReceberNome(Action):
 
     def name(self) -> Text:
@@ -16,10 +17,13 @@ class ActionReceberNome(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
+        # Preenche informações
         user_id = tracker.get_slot("user_id")
-        r = requests.get(f'http://localhost:10000/api/actions/{user_id}/simple')
+        r = requests.get(
+            f'http://localhost:10000/api/actions/{user_id}/simple')
         data = r.json()
-        return [SlotSet("first_name", data['firstName']), SlotSet("last_name", data['lastName']), SlotSet("is_actived", data['isActivated']) ,SlotSet("user_name", data['userName'])]
+        return [SlotSet("first_name", data['firstName']), SlotSet("last_name", data['lastName']), SlotSet("is_actived", data['isActivated']), SlotSet("user_name", data['userName'])]
+
 
 class ActionMostrarDados(Action):
 
@@ -31,23 +35,29 @@ class ActionMostrarDados(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         user_id = tracker.get_slot("user_id")
-        trackers = requests.get(f'http://localhost:10000/api/actions/{user_id}/trackers')
+
+        # Recebe rastreadores da aapi
+        trackers = requests.get(
+            f'http://localhost:10000/api/actions/{user_id}/trackers')
         data = trackers.json()
         tks = []
+
+        # Imprime sequencia de msg informando os dados da conta do usuario
+
         is_activated = "Sua conta está ativa"
         if (tracker.slots["is_actived"] == False):
             is_activated = "Sua conta está inativa"
 
         dispatcher.utter_template("utter_mostrar_dados", tracker)
-        dispatcher.utter_message(f"sua conta está {is_activated}")
+        dispatcher.utter_message(f"sua conta está {is_activated} \n")
 
-        dispatcher.utter_message("Seus rastreadores: ")
+        dispatcher.utter_message("Seus rastreadores: \n")
         for tk in data:
             tks.append(tk['name'])
-            dispatcher.utter_message(f"{tk['name']} - {tk['phone']} - imei- {tk['imei']}")
-            
+            dispatcher.utter_message(
+                f"{tk['name']} - {tk['phone']} - imei- {tk['imei']}" + "\n")
 
-        return [SlotSet("rastreadores", {"tk" : tks})]
+        return [SlotSet("rastreadores", {"tk": tks})]
 
 
 class ActionVerificarErros(Action):
@@ -60,7 +70,60 @@ class ActionVerificarErros(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         user_id = tracker.get_slot("user_id")
-        errorData = requests.get(f'http://localhost:10000/api/actions/{user_id}/sanity')
+
+        # Chamado de api com logica de verificação de saude da conta
+        errorData = requests.get(
+            f'http://localhost:10000/api/actions/{user_id}/sanity')
         data = errorData.json()
-        dispatcher.utter_message(f"{data}")
+
+        # recebe resultado da chamada de api e popula listas com informações de erro
+        tracker_errors_data = data["trackerError"]
+        subscription_errors_data = data["subscriptionError"]
+
+        # Formato esperado dos erros: [{"error" : string, "trackerId" : int}]
+        tracker_errors = []
+        subscription_errors = []
+
+        for index, error in enumerate(tracker_errors_data):
+            tracker_errors.append({
+                "error": error[0]["error"],
+                "trackerId": error[0]["trackerId"]
+            })
+
+        for index, error in enumerate(subscription_errors_data):
+            subscription_errors.append({
+                "error": error["error"],
+                "trackerId": error["trackerId"]
+            })
+
+        all_errors = tracker_errors + subscription_errors
+
+        if (len(all_errors) > 0 ) :
+            dispatcher.utter_message("Foram identificados os seguintes problemas:\n ")
+            for error in all_errors:
+                dispatcher.utter_message(sprintError(error, user_id) + "\n")
+        else:
+            dispatcher.utter_message("Não foi idenficado nenhum problema\n")
         return []
+
+
+# returns a string with the correct error message for that error object
+def sprintError(error, user_id):
+    error_type = error["error"]
+    tracker_id = error["trackerId"]
+
+    # Tracker error msg logic
+    # Python has no switches :(
+
+    if(tracker_id != 0):
+        tracker_name = requests.get(
+            f'http://localhost:10000/api/actions/{user_id}/trackerName/{tracker_id}')
+        if (error_type == "apn_error"):
+            return f"Erro de apn no rastreador {tracker_id} - {tracker_name.text}"
+    else:
+        if (error_type == "inactive"):
+            return "Sua assinatura está inativa."
+        if (error_type == "expired"):
+            return "Sua assinatura está expirada."
+        if (error_type == "too_many_trackers"):
+            return "Você tem mais rastreadores cadastrados em sua conta o que o permitido"
--- pad.py
+++ pad.py
@@ -1,7 +1,51 @@
 import requests
 import json
 
-userId = "3F628CB7-F0C5-4F56-A720-3F5BBD44C2EA"
-r = requests.get(f'http://localhost:10000/api/actions/{userId}/simple')
-data = r.json()
-print(data['firstName'])
+user_id = "B95153C1-8CD0-41A4-A3E2-B3394EFB9020"
+errorData = requests.get(f'http://localhost:10000/api/actions/{user_id}/sanity')
+data = errorData.json()
+
+# recebe resultado da chamada de api e popula listas com informações de erro
+
+tracker_errors_data = data["trackerError"]
+subscription_errors_data = data["subscriptionError"]
+
+
+# Formato esperado dos erros: [{"error" : string, "trackerId" : int}]
+tracker_errors = []
+subscription_errors = []
+
+for index, error in enumerate(tracker_errors_data):
+    tracker_errors.append({
+        "error" : error[0]["error"],
+       "trackerId": error[0]["trackerId"]
+    })
+    
+for index, error in enumerate(subscription_errors_data):
+    subscription_errors.append({
+        "error" : error["error"],
+       "trackerId": error["trackerId"]
+    })
+
+all_errors = tracker_errors + subscription_errors
+
+# returns a string with the correct error message for that error object
+def sprintError(error):
+    return_string = ""
+    error_type = error["error"]
+    tracker_id = error["trackerId"]
+    
+    # Tracker error msg logic
+    # Python has no switches :(
+
+    if(tracker_id != 0):
+        tracker_name = requests.get(f'http://localhost:10000/api/actions/{user_id}/trackerName/{tracker_id}')
+        if (error_type == "apn_error") : return f"Erro de apn no rastreador {tracker_id} - {tracker_name.text}"
+    else:
+        if (error_type == "inactive") : return "Sua assinatura está inativa."
+        if (error_type == "expired") : return "Sua assinatura está expirada."
+        if (error_type == "too_many_trackers") : return "Você tem mais rastreadores cadastrados em sua conta o que o permitido"
+            
+
+for error in all_errors:
+    print(sprintError(error))
